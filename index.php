a8ce6cffacf30ab41ee1248b75c1b8755b6369b2
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-02 16:34:20 +0200

    [add] print values

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
+
 require_once __DIR__ . '/Model/Movie.php';
+
+$harryPotter = new Movie('Harry Potter e la pietra filosofale', 2001, ['Daniel Radcliffe', 'Emma Watson', 'Rupert Grint'], 'en');
+
+var_dump($harryPotter)
+
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +18,23 @@ require_once __DIR__ . '/Model/Movie.php';
 </head>
 
 <body>
+
+  <div class="container">
+    <h1>Movies</h1>
+    <div class="container">
+      <div class="movie-info">
+        <h3><?php $harryPotter->getFullInfo() ?></h3>
+        <h4>Cast :</h4>
+        <ul>
+          <?php foreach ($harryPotter->cast as $actor) : ?>
+            <li>
+              <?php echo $actor ?>
+            </li>
+          <?php endforeach ?>
+        </ul>
+      </div>
+    </div>
+  </div>
 
 </body>
 
