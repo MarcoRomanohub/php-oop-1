ad96c145eafb949807ef76148da2a799e2e32c5c
MarcoRomanoHub <marcoromano270@gmail.com>
2024-05-02 16:15:16 +0200

    [add] Movie class

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,2 +1,24 @@
+<?php
 
+class Movie
+{
+  public $title;
+  public $year;
+  public $cast;
+  public $lang;
 
+  public function __construct(string $_title, int $_year, array $_cast, string $_lang)
+  {
+
+    $this->title = $_title;
+    $this->year = $_year;
+    $this->cast = $_cast;
+    $this->lang = $_lang;
+  }
+
+  public function getFullInfo()
+  {
+    // metto echo invece di return
+    echo "$this->title, $this->year, $this->lang";
+  }
+}
